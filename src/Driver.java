c6bd30b4724b088f46be9d626b5e3d42797b13b8
sydneyfarber <79294831+sydneyfarber@users.noreply.github.com>
2021-05-05 14:20:07 -0500

    Changed Stuff

--- src/Driver.java
+++ src/Driver.java
@@ -4,8 +4,14 @@ public class Driver
 
 	public static void main(String[] args) 
 	{
-		System.out.println("hello world!!!");
-
+		HearthstoneCard c1 = new HearthstoneCard("Armor Vendor", 1, 1, 3);
+		HearthstoneCard c2 = new HearthstoneCard("Wandmaker", 2, 2, 2);
+		c1.display();
+		c2.display();
+		
+		c1.setName("woot");
+		c1.display();
+		
 	}
 
 }
---
+++ src/HearthstoneCard.java
@@ -0,0 +1,30 @@
+
+public class HearthstoneCard 
+{
+	private int cost;
+	private int attack;
+	private int defense;
+	private String name;
+	
+	public HearthstoneCard(String name, int cost, int attack, int defense)
+	{
+		this.cost = cost;
+		this.attack = attack;
+		this.defense = defense;
+		this.name = name; 
+	}
+	
+	public void setName (String name)
+	{
+		if(name.length() >=5)
+		{
+			this.name = name;
+		}	
+	}
+	
+	void display()
+	{
+		//System.out.println("Name:" + this.name + "\nCost" + this.cost + "\nAttack: " + this.attack + " Defense:" + this.defense);
+		System.out.format("Name: %s \n Cost: %d \nAttack: %d Defense: %d \n", this.name, this.cost, this.attack, this.defense);
+	}
+}
