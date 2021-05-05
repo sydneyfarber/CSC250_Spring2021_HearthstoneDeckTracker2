a418a009d5497c9d17a61689d98a39e1e4245511
sydneyfarber <79294831+sydneyfarber@users.noreply.github.com>
2021-05-05 17:55:47 -0500

    updates

--- src/Driver.java
+++ src/Driver.java
@@ -6,55 +6,12 @@ public class Driver
 
 	public static void main(String[] args) 
 	{
-		HearthstoneCard c1 = new HearthstoneCard("Armor Vendor", 1, 1, 3);
-		HearthstoneCard c2 = new HearthstoneCard("Wandmaker", 2, 2, 2);
-		c1.display();
-		c2.display();
-		
-		c1.setName("woot");
-		c1.display();
-	
-		String cardJson = Driver.getJSON("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
-		System.out.println(cardJson);
-		//Let's make a json parser
-		
-		//name = Input("Enter your name:")
-		//Scanner input = new Scanner(System.in);
-		//System.out.print("Enter your age:");
-		//String age = input.nextLine(); 
-		//System.out.println(Integer.parseInt(age) + 2);
-		//int age = input.nextInt();
-		//System.out.println(age +2);
-		
-	}
-	
-	//open a URL and read its contents as a String
-	static String getJSON(String urlString) 
-	{
-		String line = "";
-		try
+		//URLReader hearthstoneURLReader = new URLReader("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
+		//hearthstoneURLReader.display(); 
+		String temp = "abc def";
+		for(int i = 0; i < temp.length(); i++)		
 		{
-			URL url = new URL(urlString);
-			Scanner input = new Scanner(url.openStream());
-			// open the url stream, wrap it up a few "readers"
-			//BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-			
-			//keep reading from the scanner as long as there is something to read
-			while (input.hasNext())
-			{
-				line += input.nextLine();
-			}
-			
-			//close out reader
-			input.close();
-			
-			//reader.close();
+			System.out.println(temp.charAt(i)); 
 		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-			line = "Can't Connect"; 
-		}
-		return line;
 	}
 }
---
+++ src/JSONParser.java
@@ -0,0 +1,16 @@
+
+public class JSONParser 
+{
+	private String theJSONString;
+	
+	public JSONParser(String theJSONString)
+	{
+		this.theJSONString = theJSONString;
+	}
+	
+	public void parse() //will eventually return a JSONObject 
+	{
+		//his job is to parse through a JSON String and return a single JSONObject
+		//that is the parsed version
+	}
+}
