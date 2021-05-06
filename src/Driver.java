872e96c48a2b6919851e4407dc98d99657776e73
sydneyfarber <79294831+sydneyfarber@users.noreply.github.com>
2021-05-05 20:02:01 -0500

    HW #7

--- src/Driver.java
+++ src/Driver.java
@@ -2,6 +2,8 @@ import java.net.URL;
 import org.json.simple.JSONValue;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 	
 public class Driver 
@@ -11,32 +13,38 @@ public class Driver
 	{
 		URLReader hearthstoneURLReader = new URLReader("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
 		//System.out.println(hearthstoneURLReader.getTheURLContents()); 
-		 Object obj = JSONValue.parse(hearthstoneURLReader.getTheURLContents()); 
+		 Object obj = JSONValue.parse(hearthstoneURLReader.getTheURLContents());
+		 //HearthstoneCard[] theMinions = new HearthstoneCard[1924];
+		 ArrayList<HearthstoneCard> theMinions = new ArrayList<HearthstoneCard>(); 
+
 		 //System.out.println(obj instanceof JSONArray);
 		 if(obj instanceof JSONArray)
-		 { 	
+		 { 
+			 
 			 // I am only in here if obj IS a JSONArray
 			 JSONArray array = (JSONArray)obj;
-			 int count = 0; 
 		
 			 for(int i = 0; i < array.size(); i++)
 			 {
 			 	JSONObject cardData = (JSONObject)array.get(i);
-			 	if(cardData.containsKey("name")&& cardData.containsKey("name"))
+			 	if(cardData.containsKey("cost")&& cardData.containsKey("name"))
 			 	{
 			 		if(cardData.containsKey("type") && cardData.get("type").equals("MINION")) 
 			 	    {
-			 			System.out.println(cardData.get("name"));
-					 	System.out.println(cardData.get("cost")); 
-					 	System.out.println(cardData.get("attack")); 
-					 	System.out.println(cardData.get("health")); 
-					 	count++;
+			 			//Only in here if a minion card
+			 			String name = (String)cardData.get("name");
+			 			int cost = Integer.parseInt(cardData.get("cost").toString());
+			 			int attack = Integer.parseInt(cardData.get("attack").toString());
+			 			int health = Integer.parseInt(cardData.get("health").toString());
+			 			HearthstoneCard temp = new HearthstoneCard(name, cost, attack, health);
+			 			theMinions.add(temp); 
+			 			temp.display();
 			 		}
-			 	
+			 		
 			 	}
 			 	
 			 } 
-		     System.out.println(count); 
+		     System.out.println(theMinions.size()); 
 		 }	 
 	}	
 } 
